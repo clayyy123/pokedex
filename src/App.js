814ca3503e135ed2556d9bec1f19f5ce599c59bd
Clayton Wang <claydwang@gmail.com>
2019-09-24 14:59:14 -0700

    grab pokemon through api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('');
+  const [poke, setPoke] = useState({});
+
+  const getPoke = async () => {
+    try {
+      const poke = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const parsed = await poke.json();
+      console.log(parsed);
+      setPoke(parsed);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const record = () => {
+    console.log(name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PokeDex</h1>
+      <input onChange={e => setName(e.target.value)} value={name} />
+      <button onClick={getPoke}>record!</button>
     </div>
   );
-}
+};
 
 export default App;
