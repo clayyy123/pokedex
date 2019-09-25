c6e360cb736b2546eb7e6a167e07ba7695a6ffaa
Clayton Wang <claydwang@gmail.com>
2019-09-25 13:19:51 -0700

    set up pagination function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
+
+const Container = styled.div`
+  border: 0.1rem solid black;
+  height: 50rem;
+  width: 40rem;
+  border-radius: 4rem;
+`;
 
 const App = () => {
   const [name, setName] = useState('');
   const [poke, setPoke] = useState({});
+  const [page, setPage] = useState({ offset: 0, limit: 10 });
+  const [pokemons, setPokemons] = useState([]);
 
   const getPoke = async () => {
     try {
@@ -16,8 +26,26 @@ const App = () => {
     }
   };
 
-  const record = () => {
-    console.log(name);
+  const getPokemons = async () => {
+    try {
+      const pokemon = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?offset=${page.offset}&limit=${page.limit}`
+      );
+      const parsed = await pokemon.json();
+      setPokemons(parsed.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const prevPage = () => {
+    try {
+    } catch (err) {}
+  };
+
+  const nextPage = () => {
+    const newP = { offset: page.offset + 10, limit: page.limit + 10 };
+    setPage(newP);
   };
 
   return (
@@ -25,6 +53,12 @@ const App = () => {
       <h1>PokeDex</h1>
       <input onChange={e => setName(e.target.value)} value={name} />
       <button onClick={getPoke}>record!</button>
+      <button onClick={getPokemons}>view pokemon</button>
+      <Container>
+        {pokemons.map((p, i) => {
+          return <div>{p.name}</div>;
+        })}
+      </Container>
     </div>
   );
 };
